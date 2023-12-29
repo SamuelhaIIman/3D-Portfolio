b3e34d85c118456c3d81278bb892fddeb099ec98
Samuel Hållman <144782909+SamuelhaIIman@users.noreply.github.com>
2023-12-29 20:16:48 +0200

    Add files via upload

---
+++ src/components/About.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+// import { motion } from "framer-motion";
+
+// import { styles } from "../styles";
+// import { services } from "../constants";
+// import { fadeIn, textVariant } from "../utils/motion";
+
+// const ServiceCard = ({ index, title, icon}) => {
+//   return (
+//     <p>{title}</p>
+//   )
+// }
+
+const About = () => {
+  return (
+    <>
+      {/* <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p variants={FileSystemDirectoryEntry("", "", 0.1, 1)}
+      className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+      My journey has been diverse—I transitioned from being a non-commissioned 
+      officer in the army, where I honed skills in team leadership and effective 
+      communication. These skills seamlessly translated into my studies, where 
+      I'm laser-focused on data processing with a keen interest in website creation. 
+      As I tread the path of continuous learning, I'm eager to contribute my skills 
+      to real-world projects and innovate in the ever-evolving web development landscape.
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div> */}
+    </>
+  )
+}
+
+export default About
