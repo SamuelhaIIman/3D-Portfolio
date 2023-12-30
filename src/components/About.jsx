fe30020a28f345f43898c13cfe6e7787b9ff207d
SamuelhaIIman <samuel.hollman@hotmail.com>
2023-12-30 19:27:02 +0200

    About & HOC
    Higher order of hierarchy updates and fixed bug about section.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,25 +1,42 @@
 import React from 'react'
-// import { motion } from "framer-motion";
+import { motion } from "framer-motion";
 
-// import { styles } from "../styles";
-// import { services } from "../constants";
-// import { fadeIn, textVariant } from "../utils/motion";
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+import { Tilt } from 'react-tilt';
+import { SectionWrapper } from "../hoc";
 
-// const ServiceCard = ({ index, title, icon}) => {
-//   return (
-//     <p>{title}</p>
-//   )
-// }
+const ServiceCard = ({ index, title, icon}) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+      variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+      className='w-full green-pink-graient p-[1px] rounded-[20px] shadow-card'
+      >
+        <div
+        options={{max: 45, scale: 1, speed: 450}}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+        >
+          <img src={icon} alt={title} 
+          className='text-white text-[20px] font-bold text-center'
+          />
+        </div>
+      </motion.div>
+
+    </Tilt>
+  )
+}
 
 const About = () => {
   return (
     <>
-      {/* <motion.div variants={textVariant()}>
+      <motion.div variants={textVariant()}>
         <p className={styles.sectionSubText}>Introduction</p>
         <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
 
-      <motion.p variants={FileSystemDirectoryEntry("", "", 0.1, 1)}
+      <motion.p variants={fadeIn("", "", 0.1, 1)}
       className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
       My journey has been diverse—I transitioned from being a non-commissioned 
@@ -34,9 +51,9 @@ const About = () => {
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
         ))}
-      </div> */}
+      </div>
     </>
   )
 }
 
-export default About
+export default SectionWrapper(About, "about");
