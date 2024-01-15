1232dce49eeaed1bb8ebb61c5adb1e7511ce9727
SamuelhaIIman <144782909+SamuelhaIIman@users.noreply.github.com>
2024-01-15 20:56:11 +0200

    Stars
    some code

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { useState, useRef, Suspense } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Points, PointMateria, Preload, Sphere } from "@react-three/drei";
+import * as random from "maath/random/dist/maath-random.esm";
+
 
 const Stars = () => {
+  const ref = useRef();
+
+  const sphere = random.inSphere(new Float32Array (5000), { radius: 1.2 })
   return (
-    <div>Stars</div>
+    <group>
+      <Points ref={ref} positions={Sphere} />
+    </group>
   )
 }
 
-export default Stars
+const StarsCanvas = () => {
+  return (
+    <div className="w-full h-full absolute inset-0 <-[-1]">
+      <canvas camera={{ posisition: [0, 0, 1]}}>
+        <Suspense fallback={null}>
+          <Stars />
+        </Suspense>
+
+      </canvas>
+    </div>
+  )
+}
+
+export default StarsCanvas;
